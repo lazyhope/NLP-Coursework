78f14717c153c56c0503cd62b603a42dee50d726
Vincent_Luo <luo793138445@gmail.com>
2025-02-27 11:56:45 +0000

    train_electra update

--- train_electra.py
+++ train_electra.py
@@ -3,7 +3,7 @@ import torch
 import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score, f1_score, precision_recall_fscore_support
-from datasets import Dataset
+from torch.utils.data import Dataset
 
 TRAIN_PATH = "data/train_data.csv"
 VAL_PATH = "data/val_data.csv"
